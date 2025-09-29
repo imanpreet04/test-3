c1d014d240a711ffa2d044c939e1dd11c67346f5
gavy04 <imanpreet@pantomax.co>
2025-09-29 11:39:57 +0530

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { Client } = require("pg");
+const app = express();
+app.use(express.json());
+
+const db = new Client({ connectionString: process.env.DATABASE_URL });
+db.connect();
+
+app.post("/user", async (req, res) => {
+  const { name } = req.body;
+  const query = `INSERT INTO users (name) VALUES ('${name}')`; 
+  res.send("User added");
+});
+
+app.post("/eval", (req, res) => {
+  const result = eval(req.body.code);
+  res.send({ result });
+});
+
+app.listen(3000, () => console.log("Server running"));
