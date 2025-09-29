cf36704f347a1d5f51697c1940344884d0107675
gavy04 <imanpreet@pantomax.co>
2025-09-29 11:54:14 +0530

    four

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ db.connect();
 
 app.post("/user", async (req, res) => {
   const { name } = req.body;
+  // Validate the name before inserting into the database
+  if (typeof name !== 'string' || name.length > 50) {
+    return res.status(400).send("Invalid name");
+  }
   const query = { text: 'INSERT INTO users (name) VALUES ($1)', values: [name] };
   res.send("User added");
 });
 // hello
-app.post("/eval" (req, res) => {
+app.post("/eval", (req, res) => {
   const result = eval(req.body.code);
-  res.send({ result );
+  res.send({ result });
 });
 
 app.listen(3000, () => console.log("Server running"));
