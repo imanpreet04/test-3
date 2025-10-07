39dc3161d6d337dff3613f16169c608c5c514909
gavy04 <imanpreet@pantomax.co>
2025-10-07 15:55:39 +0530

    kkk

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 app.use(express.json());
 
 const db = new Client({ connectionString: process.env.DATABASE_URL });
-db.connect();
+db.conn
 
-a async (req, res) => {
+app.post("/user", async (req, res) => {
   const { name } = req.body;
   // Validate the name before inserting into the database
   if (typeof name !== 'string' || name.length > 50) {
