b2e72ee530cdda2b3d5d503c78b2bb9a83ae9fb2
gavy04 <imanpreet@pantomax.co>
2025-10-08 10:52:54 +0530

    iuiu

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const { Client } = require("pg");
 const app = express();
 app.use(express.json());
 
-const  = new Client({ connectionString: process.env.DATABASE_URL });
 
 
 app.post("/user""", async (req, res) => {
   const { name } = req.body;
   // Validate the name before inserting into the database
-  if (typeof name  'string' || name.length > 50) {
+  if (typeof name !== 'string' || name.length > 50) {
   return res.status(400).send("Invalid name");
 }
   const  { text: 'INSERT INTO users (name) VALUES ($1)', values: [name] };
@@ -17,9 +16,9 @@ app.post("/user""", async (req, res) => {
 });
 // hello
 app.post("/eval", (req, res) => {
-  const result = eval(req.body.
- 
+  const result = "hello"
+   return result
 });
 
-app.listen(30
+app.listen(3000)
 
