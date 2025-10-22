70c6730a155f873d19daa6fd6cd07c1a1d21bde6
gavy04 <imanpreet@pantomax.co>
2025-10-22 16:10:27 +0530

    Fix user route definition in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 const db = new Client({ connectionString: process.env.DATABASE_URL });
 db.connect();
 
-app.post("/user", async (req, res) => {
+app.post
   const { name } = req.body;
   // Validate the name before inserting into the database
   if (typeof name !== 'string' || name.length > 50) {
